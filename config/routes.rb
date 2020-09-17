67df7dd764270b99fbd796c225383b94f8e11ceb
necocoa <43537209+necocoa@users.noreply.github.com>
2020-09-17 13:22:22 +0900

    ログイン・ログアウトのrouting変更とログアウトをget pathに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users
+  devise_for :users, skip: [:sessions]
+  as :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    match 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
+  end
 end
