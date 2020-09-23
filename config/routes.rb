6d60efa2db4b8f013f7545a51a90cf12b0e62112
necocoa <43537209+necocoa@users.noreply.github.com>
2020-09-23 15:07:28 +0900

    deviseの設定変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users, skip: [:sessions]
-  as :user do
-    get 'login', to: 'devise/sessions#new', as: :new_user_session
-    post 'login', to: 'devise/sessions#create', as: :user_session
-    match 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
-  end
+  devise_for :users, module: 'users', skip: :sessions
+  devise_for :users, module: 'users', path: '', path_names: { sign_in: 'login', sign_out: 'logout' }, only: :sessions
 end
