1611966c3c4cd19dc3fcc4eec7297b481a0c9a77
necocoa <43537209+necocoa@users.noreply.github.com>
2020-10-01 13:06:43 +0900

    post_review modelの作成

--- app/models/post_review.rb
+++ app/models/post_review.rb
@@ -1,29 +1,27 @@
 # == Schema Information
 #
-# Table name: posts
+# Table name: post_reviews
 #
 #  id         :bigint           not null, primary key
 #  body       :text             default(""), not null
-#  title      :string           default(""), not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  post_id    :bigint           not null
 #  user_id    :bigint           not null
 #
 # Indexes
 #
-#  index_posts_on_created_at  (created_at)
-#  index_posts_on_user_id     (user_id)
+#  index_post_reviews_on_post_id  (post_id)
+#  index_post_reviews_on_user_id  (user_id)
 #
 # Foreign Keys
 #
+#  fk_rails_...  (post_id => posts.id)
 #  fk_rails_...  (user_id => users.id)
 #
-class Post < ApplicationRecord
+class PostReview < ApplicationRecord
+  belongs_to :post
   belongs_to :user
-  has_many :post_reviews, dependent: :destroy
 
-  validates :title, presence: true, length: { in: 5..100 }
   validates :body, presence: true, length: { in: 10..10000 }
-
-  scope :recent, -> { order(created_at: :desc) }
 end
