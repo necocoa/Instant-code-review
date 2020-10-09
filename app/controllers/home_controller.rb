b1ae6d54fb14d1e5f324b0afdb95350304882c43
necocoa <43537209+necocoa@users.noreply.github.com>
2020-10-09 18:34:32 +0900

    topにはopstsを表示するようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   # GET /
-  def index; end
+  def index
+    @posts = Post.includes(:user).recent.page(params[:page])
+    render 'posts/index'
+  end
 end
